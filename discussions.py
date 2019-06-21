e7fadfdf0ed4f0c958bcfddd9a9170c1f80e582b
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-06-21 23:47:08 +1200

    Update discussions.py

--- discussions.py
+++ discussions.py
@@ -2,6 +2,24 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
+from env import token
+import requests
+
+def sendSlack():
+    print('skac')
+    my_file = {
+        'file' : ('./image_0.png', open('./image_0.png', 'rb'), 'png')
+    }
+
+    payload={
+        "filename":"image_0.png", 
+        "token": token, 
+        "channels":['#hi'], 
+    }
+
+    r = requests.post("https://slack.com/api/files.upload", params=payload, files=my_file)
+    print(r.status_code)
+
 
 # set up driver & get facebook
 chrome_options = webdriver.ChromeOptions()
@@ -27,19 +45,23 @@ if authors:
 
             # expand post if it exists
             try:
-                print('hitting try')
                 link = driver.find_element_by_link_text('Expand Post')
                 link.click()
                 driver.save_screenshot('image_' + str(count) + '.png')
                 count += 1
+                sendSlack()
+
             except :
-                print('hitting except')
                 driver.save_screenshot('image_' + str(count) + '.png')
                 count += 1
+                sendSlack()
+
         else: pass
+    
             
         
         
 else:
+
     driver.close()
 
